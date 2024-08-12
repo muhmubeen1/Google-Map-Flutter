332cdd4f54dc6450491d87e754c67a8fae6945ce
muhmubeen1 <muhmubeen1@gmail.com>
2024-08-12 21:18:30 +0500

    add markers

--- googlemap/lib/home_page.dart
+++ googlemap/lib/home_page.dart
@@ -16,6 +16,26 @@ class _HomePageState extends State<HomePage> {
     target: LatLng(37.4220, -122.0841), // Coordinates for the Googleplex
     zoom: 14,
   );
+  List<Marker> _marker = [];
+  List<Marker> _list = const [
+    Marker(
+      markerId: MarkerId("1"),
+      position: LatLng(37.4220, -122.0841),
+      infoWindow: InfoWindow(title: "Current Location"),
+    ),
+    Marker(
+      markerId: MarkerId("2"),
+      position: LatLng(37.56, -122.0753),
+      infoWindow: InfoWindow(title: "Second Location"),
+    ),
+  ];
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    _marker.addAll(_list);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,10 +44,11 @@ class _HomePageState extends State<HomePage> {
         body: SafeArea(
             child: GoogleMap(
           initialCameraPosition: _kGooglePlex,
-          mapType: MapType.satellite,
+          mapType: MapType.normal,
           myLocationButtonEnabled: true,
           myLocationEnabled: true,
           compassEnabled: true,
+          markers: Set<Marker>.of(_marker),
           onMapCreated: (GoogleMapController controller) {
             _controller.complete(controller);
           },
