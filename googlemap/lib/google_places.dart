f68712055f9240299e2e7cd86939df06c1e4c29c
muhmubeen1 <muhmubeen1@gmail.com>
2024-08-16 23:54:38 +0500

    Add google places

---
+++ googlemap/lib/google_places.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class GooglePlaces extends StatefulWidget {
+  const GooglePlaces({Key? key}) : super(key: key);
+  @override
+  State<GooglePlaces> createState() => _GooglePlacesState();
+}
+
+class _GooglePlacesState extends State<GooglePlaces> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
