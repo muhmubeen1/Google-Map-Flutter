45cdf119dc6cfd5ef2eb21dd814ae445534d92b9
muhmubeen1 <muhmubeen1@gmail.com>
2024-08-22 22:24:44 +0500

    add custom markers

--- googlemap/lib/custom_markers.dart
+++ googlemap/lib/custom_markers.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -9,8 +11,11 @@ class CustomMarkers extends StatefulWidget {
 }
 
 class _CustomMarkersState extends State<CustomMarkers> {
+  final Completer<GoogleMapController> _controller = Completer();
   static const _initailCamera =
       CameraPosition(target: LatLng(35.682839, 139.759455), zoom: 14);
+  List<String> images = ["image/car.png", "image/car1.png", "image/car2.png"];
+  List<Marker> _marker = [];
   List<Marker> _list = const [
     Marker(
       markerId: MarkerId("1"),
@@ -24,10 +29,16 @@ class _CustomMarkersState extends State<CustomMarkers> {
     ),
     Marker(
       markerId: MarkerId("1"),
-      position: LatLng(35.658580, 139.745433),
-      infoWindow: InfoWindow(title: "Japan Tower"),
+      position: LatLng(35.685175, 139.745433),
+      infoWindow: InfoWindow(title: "Imperial Palace"),
     )
   ];
+  @override
+  void initState() {
+    // TODO: implement initState
+    _marker.addAll(_list);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -36,6 +47,8 @@ class _CustomMarkersState extends State<CustomMarkers> {
         child: GoogleMap(
           initialCameraPosition: _initailCamera,
           mapType: MapType.normal,
+          markers: Set<Marker>.of(_list),
+          onMapCreated: _controller.complete,
         ),
       )),
     );
